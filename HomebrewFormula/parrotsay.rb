995f828b8a574ac9cbaa1a217fe0fc10599743fd
Chip Wasson <chip@wasson.io>
2017-05-24 19:09:38 -0600

    Add v0.1.0 of parrotsay

--- HomebrewFormula/parrotsay.rb
+++ HomebrewFormula/parrotsay.rb
@@ -5,8 +5,8 @@
 class Parrotsay < Formula
   desc "make the parrot sing"
   homepage "https://github.com/chpwssn/parrot-bash"
-  url "https://github.com/chpwssn/parrot-bash/archive/v0.0.3.tar.gz"
-  sha256 "ca108af8e0e251588890a98ef457f373ac5795199306247f0f86769098812c3e"
+  url "https://github.com/chpwssn/parrot-bash/archive/v0.1.0.tar.gz"
+  sha256 "5a10fc77cdca78c12fee39c8c00e157af2209af8949682b89374b19d9ff904eb"
 
   def install
     bin.install "parrotsay"
