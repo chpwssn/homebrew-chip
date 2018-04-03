d5dd56589290c813f2ccd6d6ba07cd5686b37253
Chip Wasson <chip@wasson.io>
2018-04-03 17:01:38 -0600

    Bump version

--- HomebrewFormula/binsio-bash.rb
+++ HomebrewFormula/binsio-bash.rb
@@ -1,8 +1,8 @@
 class BinsioBash < Formula
   desc "A bins.io command line client"
   homepage "bins.io"
-  url "https://github.com/chpwssn/binsio-bash/archive/v0.3.tar.gz"
-  sha256 "cbe29af7851d8aad649ac2188058c8f206cad5397e374eed70cfce802353d72f"
+  url "https://github.com/chpwssn/binsio-bash/archive/v0.3.1.tar.gz"
+  sha256 "19d66863232c2ecaf9b668e95350e5a0c81e423c36097a82c736eac06d5ee341"
 
   def install
     bin.install "binsio"
